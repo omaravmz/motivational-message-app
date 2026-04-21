d3b4f5d7f253424268e5a8a945b4cb0a1478f772
omaravmz <omar.avila4@outlook.com>
2026-04-21 09:30:02 -0600

    Deploy pipeline

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
